380ae7aeffa3c6f4cccca058afe36fd7984d2ad7
Parth Sharma <parthsharmaclg@gmail.com>
2025-04-27 16:26:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Lifepulse backend is live!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
